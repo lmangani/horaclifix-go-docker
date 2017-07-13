fa2bbdcdb78ddc4d371102865420570584b033cc
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2017-07-13 21:35:54 +0200

    resolve host to domain for ranch/k8s setups

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-echo "HORACLIFIX GO Starting"
-echo "Shipping HEP to: $HEP_HOST:$HEP_PORT"
+echo "HORACLIFIX GO Starting..."
 
-horaclifix -H $HEP_HOST:$HEP_PORT
+if [[ $HEP_HOST =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Shipping HEP to: $HEP_HOST:$HEP_PORT"
+  horaclifix -H $HEP_HOST:$HEP_PORT
+else
+  HEP_IP=$(ping -q -c 1 -t 1 $HEP_HOST | grep PING | sed -e "s/).*//" | sed -e "s/.*(//")
+  echo "Shipping HEP to: $HEP_IP:$HEP_PORT"
+  horaclifix -H $HEP_IP:$HEP_PORT
+fi
