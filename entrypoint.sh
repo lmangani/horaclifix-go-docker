c9a94f04228e6edadd6c011dc06c43d4ba5c1d78
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2017-07-14 09:22:57 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 echo "HORACLIFIX GO Starting..."
 
+COMMAND="horaclifix "
+
 if [[ $HEP_HOST =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  echo "Shipping HEP to: $HEP_HOST:$HEP_PORT"
-  horaclifix -H $HEP_HOST:$HEP_PORT
+  echo "Adding HEP target: $HEP_HOST:$HEP_PORT"
+  COMMAND+="-H $HEP_HOST:$HEP_PORT "
 else
   HEP_IP=$(ping -q -c 1 -t 1 $HEP_HOST | grep PING | sed -e "s/).*//" | sed -e "s/.*(//")
-  echo "Shipping HEP to: $HEP_IP:$HEP_PORT"
-  horaclifix -H $HEP_IP:$HEP_PORT
+  echo "Adding HEP target: $HEP_IP:$HEP_PORT"
+  COMMAND+="-H $HEP_IP:$HEP_PORT "
 fi
+
+
+if [[ ${GRAYLOG_URL:+1} ]] ; then
+  echo "Adding Graylog target: $GRAYLOG_URL"
+  COMMAND+="-g $GRAYLOG_URL "
+fi
+
+
+if [[ ${STATSD_URL:+1} ]] ; then
+  echo "Adding Statsd target: $STATSD_URL"
+  COMMAND+="-s $STATSD_URL "
+fi
+
+echo "Executing: $COMMAND "
+/bin/bash -c "$COMMAND"
